52b7a7f888ba162412ba7a2e65b31c9346469fff
rayto1224 <rayto1224@gmail.com>
2025-12-24 14:36:26 +0800

    Step 5: doctor

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clinic',
+        'USER': 'postgres',
+        'PASSWORD': '0070',
+        'HOST': 'localhost'
     }
 }
 
--- listings/views.py
+++ listings/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def listings(request):
+    print(request,request.path)
     return render(request,'listings/listings.html')
 
 def listing(request,listing_id):
+    print(request,request.path)
     return render(request,'listings/listing.html')
 
 def search(request):
+    print(request,request.path)
     return render(request,'listings/search.html')
