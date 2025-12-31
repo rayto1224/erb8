b6129fa7f8cb1f06d8b3d3d6e944af5f7b37a9bf
rayto1224 <rayto1224@gmail.com>
2025-12-31 11:46:30 +0800

    Step 9 : about

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from listings.models import Listing
-
+from doctors.models import Doctor
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {"listings": listings}
     return render(request,'pages/index.html', context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    doctors = Doctor.objects.order_by("-hire_date")[:3]
+    mvp_doctors = Doctor.objects.all().filter(is_mvp=True)
+    context = {"doctors":doctors, "mvp_doctors":mvp_doctors}
+    return render(request,'pages/about.html',context)
